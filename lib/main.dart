e4c6606cc492abaf31c29392990565290065b93f
Changhogod <younch8342@naver.com>
2023-06-30 22:30:57 +0900

    simple home design with flutter

--- lib/main.dart
+++ lib/main.dart
@@ -10,18 +10,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar( title : Text('앱임')),
-        body: Column(
-          children: [
-            Icon(Icons.star),
-            Icon(Icons.star),
-          ],
-        )
-
-
-      )
-    );
+        home: Scaffold(
+            appBar: AppBar(title: Text('앱임')),
+            body: Text('안녕'),
+            bottomNavigationBar: BottomAppBar(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(Icons.call),
+                  Icon(Icons.message),
+                  Icon(Icons.account_box),
+                ],
+              )
+            )));
   }
 }
-
